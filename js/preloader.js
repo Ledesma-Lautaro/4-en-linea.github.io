e800b9b58e73787c57a86e9728854ba513b51325
Franco <francokain00@gmail.com>
2022-10-14 00:40:43 -0300

    carrusel completo, acomodo de botones

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,8 @@
 var percentage = document.querySelector(".percentage");
+let body = document.body
 window.addEventListener("load", progressTime);
 let contador = 0;
+body.setAttribute("style", "overflow: hidden");
 function progressTime() {
      timeset = setInterval(progressNumber, 50);
 }
@@ -11,8 +13,10 @@ function progressNumber() {
     if (contador < 100) {
         contador++;
         percentage.innerHTML = contador + "%";
+        body.setAttribute("style", "overflow: hidden");
     }
     else{
         clearInterval(timeset);
+        body.setAttribute("style", "overflow: visible");
     }
 }
