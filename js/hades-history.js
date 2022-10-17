0f7a1ab75a1b5f6220b57c6b69c1bb05bb4d1ef4
Franco <francokain00@gmail.com>
2022-10-16 23:56:29 -0300

    Terminado slider Hades

--- js/hades-history.js
+++ js/hades-history.js
@@ -7,31 +7,21 @@ for (let i = 0; i < buttons.length; i++) {
     let button = buttons[i];
     button.addEventListener("click", ()=> {
         if ((i == 0)&&(!card.classList.contains("card-hades1"))) {
-            card.classList.add("slideout");
             card.classList.remove("card-hades2");
             card.classList.remove("card-hades3");
             card.classList.add("card-hades1");
-            card.classList.add("slidein");
-            card.classList.remove("slideout");
         }
         
         else if ((i == 1)&&(!card.classList.contains("card-hades2"))) {
-            card.classList.add("slideout");
             card.classList.remove("card-hades1");
             card.classList.remove("card-hades3");
             card.classList.add("card-hades2");
-            card.classList.add("slidein");
-            
-            card.classList.remove("slideout");  
         }
         
         else if ((i == 2)&&(!card.classList.contains("card-hades3"))) {
-            card.classList.add("slideout");
             card.classList.remove("card-hades1");
             card.classList.remove("card-hades2");
             card.classList.add("card-hades3");
-            card.classList.add("slidein");
-            card.classList.remove("slideout");
         }
 })
 }
