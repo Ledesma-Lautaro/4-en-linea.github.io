bc61898c851e6f6d839ddc4a656ade56fe63dc27
Lautaro Ledesma <strangername02@gmail.com>
2022-11-26 13:35:25 -0300

    falta terminar el 9
    Co-authored-by: FrancoKain <FrancoKain@users.noreply.github.com>

--- js/characters.js
+++ js/characters.js
@@ -1,14 +1,48 @@
 let cards = document.querySelectorAll(".cards-personajes");
+let card1 = document.querySelector("#card1");
+let card2 = document.querySelector("#card2");
+let card3 = document.querySelector("#card3");
+let card4 = document.querySelector("#card4");
+let card5 = document.querySelector("#card5");
+let card6 = document.querySelector("#card6");
+let card7 = document.querySelector("#card7");
+const arrayCards = [];
+arrayCards.push(card1, card2, card3, card4, card5, card6, card7);
+let titles = document.querySelectorAll(".character-title");
+
+
 // hay que ponerle a cada card un id, para llamarlo desde la funcion
+//750 final
+//600 mitad
 
 
 window.addEventListener('scroll', () =>{
-    for (let index = 0; index < cards.length; index++) {
-        const card = cards[index];
+    const scrolled = window.scrollY;
+    if(scrolled > 500 && scrolled < 600){
+    for (let index = 0; index < arrayCards .length; index++) {
+        console.log(arrayCards);
+        const card = arrayCards[index];
         setTimeout(function(){
             //en vez de card, va a tener que ser card 1, etc, dependiendo del id, para que se muestren
             //de manera individual
-            card.setAttribute("style", "animation: 1.5s linear 0.5s forwards fade-up;"); 
+            card.setAttribute("style", 'animation: 1s linear '+ (0.25 * index) +'s forwards fade-up;'); 
+            for (let i = 0; i < titles.length; i++) {
+                const title = titles[i];
+                title.setAttribute("style", 'animation: 1s linear 0.5s forwards first-fade-right;'); 
+            }
         }, 1000);
+    }
+    }
+    if(scrolled > 600 && scrolled < 750){
+        for (let i = 0; i < titles.length; i++) {
+            const title = titles[i];
+            title.setAttribute("style", 'animation: 1s linear 0.5s forwards second-fade-right;'); 
+        }
+    }
+    if(scrolled > 750){
+        for (let i = 0; i < titles.length; i++) {
+            const title = titles[i];
+            title.setAttribute("style", 'animation: 1s linear 0.5s forwards third-fade-right;'); 
+        }
     }
 })
--- js/parallax.js
+++ js/parallax.js
@@ -1,3 +1,29 @@
-document.querySelector("#logo").addEventListener("scroll", ()=>{
 
+let fondo2 = document.querySelector("#fondo-2");
+let fondo1 = document.querySelector("#fondo-1");
+let suelo2 = document.querySelector("#suelo-2");
+let suelo1 = document.querySelector("#suelo-1");
+let humo = document.querySelector("#humo");
+let serpiente2 = document.querySelector("#serpiente-2");
+let serpiente1 = document.querySelector("#serpiente-1");
+let zagreo = document.querySelector("#zagreo");
+let logo = document.querySelector("#hadeslogo");
+
+
+
+window.addEventListener("scroll", function(){
+var value = window.scrollY;
+
+fondo2.style.top = value *0.1 + "px";
+fondo1.style.top = value *0.2 + "px";
+suelo2.style.top = value *0.3 + "px";
+suelo1.style.top = value *0.4 + "px";
+humo.style.top = value *0.5 + "px";
+serpiente2.style.top = value *0.6 + "px";
+serpiente1.style.top = value *0.7 + "px";
+zagreo.style.top = value *0.8 + "px";
+
+logo.style.opacity = 0.8- + window.pageYOffset/550 + '';
+logo.style.top = +window.pageXOffset+'px';
+logo.style.backgroundPositionY = +window.pageXOffset/2 + 'px';
 }) 
